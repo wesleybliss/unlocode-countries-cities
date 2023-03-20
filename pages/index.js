d21c3f929cb42d5b000fe06c1d29f881744ad508
Wesley Bliss <wesley.bliss@gmail.com>
2023-03-20 02:07:11 -0500

    Typo

--- pages/index.js
+++ pages/index.js
@@ -18,9 +18,6 @@ export default function Home() {
             </Head>
             <main className={styles.main}>
                 <div className={styles.description}>
-                    <div className="">
-                        <pre><code>{JSON.stringify(data, null, 4)}</code></pre>
-                    </div>
                     <div>
                         <a
                             href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
